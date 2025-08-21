0a40ca127f6e4b70f13822ae33b2731935389a12
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-08-21 10:43:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  event.preventDefault();
+  let r = parseFloat(document.getElementById("radius").value);
+  let volume;
 
+  if (isNaN(r) || r < 0) {
+    volume = NaN;
+  } else {
+    volume = (4/3) * Math.PI * Math.pow(r, 3);
+    volume = volume.toFixed(4);
+  }
+  document.getElementById("volume").value = volume;
+  }  
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
